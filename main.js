612e42cf49251b5d269d8c9f73dacc5643a4ca31
haoK05 <magicmath2k5@gmail.com>
2025-10-20 00:15:07 +0700

    init

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+document.addEventListener("click", function () {
+  const audio = document.getElementById("bg-music");
+  if (audio.paused) {
+    audio.play().catch(err => console.log("Không thể tự phát nhạc:", err));
+  }
+}, { once: true });
